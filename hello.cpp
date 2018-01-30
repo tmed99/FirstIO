0ea06692e6e78017166d9b68f75b3b41b1f141df
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-01-29 17:56:09 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -3,8 +3,9 @@
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Tanya." << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
+
     return 0;
 }
